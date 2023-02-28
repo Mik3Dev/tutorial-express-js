b99c2d9aec3364e94a1d593e222b2326ec13b7f9
mik3dev <miguel.acosta1978@gmail.com>
2023-02-27 21:40:09 -0300

    Update: Mongo data persistence

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const mongoose = require("mongoose");
+const Todo = require("./todoSchema");
 
 const PORT = 3000;
-let todos = [
+
+mongoose.connect(
+  "mongodb://user:passwowrd@localhost:27017/?authMechanism=DEFAULT",
   {
-    id: 1,
-    title: "Todo One",
-    completed: false,
-    description: "This is the first todo",
-  },
-  {
-    id: 2,
-    title: "Todo Two",
-    completed: false,
-    description: "This is the second todo",
-  },
-  {
-    id: 3,
-    title: "Todo Tree",
-    completed: false,
-    description: "This is the third todo",
-  },
-  {
-    id: 4,
-    title: "Todo four",
-    completed: false,
-    description: "This is the fourth todo",
-  },
-];
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.use(bodyParser.json());
 
@@ -36,75 +20,77 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-app.get("/api/todos", (req, res) => {
+// Listar todos los todos
+app.get("/api/todos", async (req, res) => {
   // Hacer una consulta a la base de datos
   // Listar todos los todos
-  res.json(todos);
+  try {
+    const todos = await Todo.find({});
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json({ message: "Error getting todos" });
+  }
 });
 
-app.post("/api/todos", (req, res) => {
+// Crear un todo
+app.post("/api/todos", async (req, res) => {
   const todo = req.body;
 
   // Registrar todo en base de datos
-  todo.id = todos.length + 1;
-  todo.completed = false;
-  todos.push(todo);
-
-  res.json(todo);
+  try {
+    const newTodo = new Todo({
+      title: todo.title,
+      description: todo.description,
+    });
+    await newTodo.save();
+    res.json(newTodo);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating todo" });
+  }
 });
 
-app.get("/api/todos/:id", (req, res) => {
+// Obtener un todo
+app.get("/api/todos/:id", async (req, res) => {
   const id = req.params.id;
 
   // Consultar a la base de datos por el id
-  const todo = todos.find((element) => {
-    return element.id == id;
-  });
+  try {
+    const todo = await Todo.findById(id);
 
-  if (todo === undefined) {
-    res.status(404).json({ message: "Todo not found" });
-    return;
+    if (todo === null || todo === undefined) {
+      res.status(404).json({ message: "Todo not found" });
+      return;
+    }
+
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({ message: "Error getting todo" });
   }
-
-  res.json(todo);
 });
 
-app.put("/api/todos/:id", (req, res) => {
+// Actualizar un todo
+app.put("/api/todos/:id", async (req, res) => {
   const id = req.params.id;
   const body = req.body;
 
-  const todo = todos.find((element) => {
-    return element.id == id;
-  });
-
-  if (todo === undefined) {
-    res.status(404).json({ message: "Todo not found" });
-    return;
+  try {
+    const todo = await Todo.findByIdAndUpdate(id, body, { new: true });
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating todo" });
   }
-
-  todo.title = body.title;
-  todo.description = body.description;
-  todo.completed = body.completed;
-
-  res.json(todo);
 });
 
-app.delete("/api/todos/:id", (req, res) => {
+// Eliminar un todo
+app.delete("/api/todos/:id", async (req, res) => {
   const id = req.params.id;
-  const todo = todos.find((element) => {
-    return element.id == id;
-  });
 
-  if (!todo) {
-    res.status(404).json({ message: "Todo not found" });
-    return;
+  try {
+    await Todo.findByIdAndDelete(id);
+    res.json({ message: "Todo deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting todo" });
   }
-
-  todos = todos.filter((element) => {
-    return element.id != id;
-  });
-
-  res.json({ message: "Todo deleted" });
 });
 
 app.listen(PORT, () => {
